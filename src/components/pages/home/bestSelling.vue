dc40bde9b12eb7c7734edfccb88a4e1b08e0ba10
evr.onen <evr.onengmail.com>
2023-09-11 14:38:58 +0300

    home done

--- src/components/pages/home/bestSelling.vue
+++ src/components/pages/home/bestSelling.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="trendsProduct wrapper">
+	<div class="bestSelling wrapper">
 		<div class="header flex flex-col items-center">
-			<p class="font-condensed uppercase text-center text-sm font-normal tracking-wide">popular products</p>
-			<p class="uppercase font-cinzel text-5xl mt-2 text-center">trending now</p>
+			<p class="font-condensed uppercase text-center text-sm font-normal tracking-wide">shop</p>
+			<p class="uppercase font-cinzel text-5xl mt-2 text-center">best selling</p>
 			<div class="w-12 border-b border-black mt-4"></div>
 		</div>
 		<div class="products wrapper mt-10 grid md:grid-cols-4 grid-cols-2 gap-4">
@@ -17,8 +17,8 @@
 import ProductCardUI from "../../ui/ProductCard/ProductCardUI.vue";
 import { ref } from "vue";
 
-// ** I actually need to fetch the ids according to the data coming from the table on a time basis. This is what I do to save time.
-const trendProducts = [1, 5, 7, 10];
+// ** I actually need to fetch the ids according to the data coming from the table on sell quantity. This is what I do to save time.
+const trendProducts = [1, 2, 4, 6];
 
 const props = defineProps(["products"]);
 let products = ref(props.products);
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,8 +6,14 @@
 	</div>
 	<div class="w-full max-w-[81.25rem] brandSlider px-4"><sliderHome :images="images" /></div>
 
-	<div class="trends mt-20 mb-40 px-4">
+	<div class="trends mt-20 px-4">
 		<TrendsProduct :products="productItems" />
+	</div>
+	<div class="best mt-20 px-4">
+		<BestSelling :products="productItems" />
+	</div>
+	<div class="chooseUs mt-20 px-4 w-full">
+		<ChooseUs />
 	</div>
 </template>
 
@@ -18,6 +24,8 @@ import { products } from "@/constant/products";
 import TopImage from "../components/pages/home/topImage.vue";
 import sliderHome from "../components/pages/home/sliderHome.vue";
 import TrendsProduct from "../components/pages/home/trendsProduct.vue";
+import BestSelling from "../components/pages/home/bestSelling.vue";
+import ChooseUs from "../components/pages/home/chooseUs.vue";
 
 const productItems = ref(products);
 
